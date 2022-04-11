2a9e34bcadebf35537bbcd1d5f54935a5de76a30
bradley-us <bradleydev101@gmail.com>
2022-04-11 21:58:21 +0100

    Webpack done and TSOA Done

--- src/controller/HelloController.ts
+++ src/controller/HelloController.ts
@@ -1,8 +1,16 @@
 import { BasicResponse } from './types'
 import { IHelloController } from './interfaces'
 import { LogSuccess } from '../utils/logger'
+import { Get, Query, Route, Tags } from 'tsoa'
 
+@Route('/api/hello')
+@Tags('HelloController')
 export class HelloController implements IHelloController {
+    /**
+     * Endpoint to retrieve a Message "Hello {name}" in JSON
+     * @param { string | undefined } name Name of user to be greeted
+     * @returns { BasicResponse } Promise of BasicResponse
+     */
     public async getMessage (name?: string): Promise<BasicResponse> {
         LogSuccess('[api/hello] Get Request')
         return {
